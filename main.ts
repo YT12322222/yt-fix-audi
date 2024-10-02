a3d15193f8800656d536d99b6c4a4d08ae1cde4c
YT12322222 <youssefmtantawi@gmail.com>
2024-10-02 19:53:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Chessboard)
+    basic.showString("process done")
+})
+input.onGesture(Gesture.FreeFall, function () {
+    music.play(music.stringPlayable("C5 C5 C5 C5 C5 C5 C5 C5 ", 120), music.PlaybackMode.UntilDone)
+})
 basic.forever(function () {
     basic.showString("please put this little device in Audi")
     basic.showIcon(IconNames.Target)
